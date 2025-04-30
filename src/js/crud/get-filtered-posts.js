6d3fee5c880ff4a35e9986d8de3f283c7e29f557
elen-purple <anna.havrylova.2011@gmail.com>
2025-04-30 19:14:03 +0300

    try 4

--- src/js/crud/get-filtered-posts.js
+++ src/js/crud/get-filtered-posts.js
@@ -1,8 +1,11 @@
 export const getFilteredPosts = async (key, value, page) => {
   const posts = await fetch(
     `https://680dfecfc47cb8074d91bfc4.mockapi.io/mini-project/posts`
-  ).then((reponse) => reponse.json());
-  if (posts.filter((post) => post.tag.includes(value)).length > 0) {
+    ).then((reponse) => reponse.json());
+  if (
+    posts.filter((post) => post.tag.toLowerCase().includes(value.toLowerCase()))
+      .length > 0
+  ) {
     try {
       return await fetch(
         `https://680dfecfc47cb8074d91bfc4.mockapi.io/mini-project/posts?l=8&p=${page}&${key}=${value}`
